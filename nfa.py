b435e3670d75f100e264ad488d20ce0e516c4c04
Adrian Smith <adrianrsmith93@gmail.com>
2018-05-13 16:56:55 -0700

    cool shit download now free money today

---
+++ nfa.py
@@ -0,0 +1,30 @@
+class Nfa:
+    def __init__(self):
+        self.currentStates = [] # old-state type
+        self.nextStates = [] # transition state type
+
+    def consumeInput(self, inputArr):
+        if len(inputArr) != 2:
+            raise ValueError('Incorrect parameter for inputArr. Should be int array of length 2.')
+
+        baseState = TransitionState()
+
+        # Look at each old state
+        for oldState in self.currentStates:
+            # Concat on transition states with horizontal lines
+            transStates = baseState.addHorizontal(oldState)
+
+            # Remove duplicates in transStates
+
+            tempArr = []
+            for elem in transStates:
+                tempArr = tempArr + elem.addVerticle(oldState)
+
+            # Remove duplicates in tempArr
+
+            # Concat next states with new states
+            self.nextStates = self.nextStates + tempArr[:]
+
+        # Copy next states into current states, clear next states
+        self.currentStates = self.nextStates[:]
+        self.nextStates = []
