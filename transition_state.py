761ba313b745e0ced40879da6bb850d640ab2c74
Ryan Yu <yuryan45@gmail.com>
2018-05-14 10:54:51 -0700

    add vertical updated

--- transition_state.py
+++ transition_state.py
@@ -21,7 +21,7 @@ class TransitionState:
             self.lineV = lv
         if ep is not None and len(ep) == 3:
             self.pips = ep
-        
+
         self.lineSegments = ls
 
     def makeOldState(self):
@@ -31,23 +31,36 @@ class TransitionState:
     def addHorizontal(self):
 
     def addVerticalLines(self):
+        # TODO: test this function. 
         newStates = []
 
         for i in range(len(self.lineV)):
-            if self.values[i] == 0 or self.lineV[idx]:
+            if newTState.values[i] == 0 or newTState.lineV[i] == 0:
                 return
             if not self.pips[0] and not self.pips[1] and not self.pips[2]:
+                # dot => 0, so if every pip is a dot
                 return
 
-            newTState = TransitionState(self.values, self.lineH, self.lineV, self.pips)
+            newTState = TransitionState(self.values, self.lineH, self.lineV, self.pips, self.lineSegments)
             newTState.lineV[i] = True
             newTState.values[i] -= 1
-            if newTState.lineH[i]:
-                newTState.pips[i] = False
-            if newTState.lineH[i+1]:
-                newTState.pips[i+1] = False
-            if newTState.lineV[0] and newTState.lineV[1]:
-                newTState.pips[1] = False
+            if newTState.pips[i] == endpoint: # if current pip is an endpoint, it becomes a dot
+                newTState.pips[i] = dot
+            if newTState.pips[i] == startpoint: # if currrent pip is a startpoint, it becomes an endpoint
+                newTState.pips[i] = endpoint
+
+            if newTState.pips[i+1] == endpoint: # sames goes for connecting pip
+                newTState.pips[i+1] = dot
+            if newTState.pips[i+1] == startpoint:
+                newTState.pips[i+1] = endpoint
+
+            if newTState.pips[i] == endpoint and newTState.pips[i+1] == endpoint:
+                newTState.lineSegments -= 1
+            if newTState.pips[i] == startpoint and newTState.pips[i + 1] == startpoint:
+                newTState.lineSegments += 1
+
+            if newTState.lineSegments > 3:
+                return
 
             newStates.append(newTState)
 
